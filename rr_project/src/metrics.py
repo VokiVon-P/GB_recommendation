4ff1bf5ee46963d880f4f69c428e70a6f384572f
VokiVon <vokivon.p@gmail.com>
2020-08-02 13:19:13 +0300

    Обновил функции метрик

--- rr_project/src/metrics.py
+++ rr_project/src/metrics.py
@@ -30,10 +30,30 @@ def precision_at_k(recommended_list, bought_list, k=5):
 
 
 def money_precision_at_k(recommended_list, bought_list, prices_recommended, k=5):
-    # your_code
-    # Лучше считать через скалярное произведение, а не цикл
+    """
 
-    return precision
+    @param recommended_list:
+    @param bought_list:
+    @param prices_recommended:
+    @param k:
+    @return:
+    """
+    bought_list = np.array(bought_list)
+    recommended_list = np.array(recommended_list)
+    
+    recommended_list = recommended_list[:k]
+    prices_recommended = np.array(prices_recommended[:k])
+    
+    # пойдем от обратного - проверим рекомендованный лист в покупках
+    flags = np.isin(recommended_list, bought_list)
+        
+    # получим цены купленных товаров
+    money_flags = flags*prices_recommended
+    
+    # отнесем сумму купленных к сумме рекомендованных (не учитывает количество)
+    money_precision = money_flags.sum() / prices_recommended.sum()
+    
+    return money_precision
 
 
 def recall(recommended_list, bought_list):
@@ -62,6 +82,16 @@ def recall_at_k(recommended_list, bought_list, k=5):
 
 
 def money_recall_at_k(recommended_list, bought_list, prices_recommended, prices_bought, k=5):
-    # your_code
-
-    return recall
+    bought_list = np.array(bought_list)
+    prices_bought = np.array(prices_bought)
+    
+    recommended_list = np.array(recommended_list[:k])
+    prices_recommended = np.array(prices_recommended[:k])
+    
+    flags = np.isin(bought_list, recommended_list) 
+    # флаги взвешенные на цену
+    money_flags = flags*prices_bought
+    
+    money_recall = money_flags.sum() / prices_recommended.sum()
+    
+    return money_recall
